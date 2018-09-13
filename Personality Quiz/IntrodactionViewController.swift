ffe7b74e56988089a5d1e1d1dc16210b4982d933
TimurSaidov <tim77777@inbox.ru>
2018-09-13 16:52:39 +0300

    Initial Commit

---
+++ Personality Quiz/IntrodactionViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Personality Quiz
+//
+//  Created by Timur Saidov on 13.09.2018.
+//  Copyright © 2018 Timur Saidov. All rights reserved.
+//
+
+import UIKit
+
+class IntrodactionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
