89ef7b50004749e1c90c20c17ba3f4663bdee564
TimurSaidov <tim77777@inbox.ru>
2018-09-14 17:55:06 +0300

    Implemented actions for answers

--- Personality Quiz/QuestionViewController.swift
+++ Personality Quiz/QuestionViewController.swift
@@ -23,12 +23,64 @@ class QuestionViewController: UIViewController {
     @IBOutlet weak var multipleLable2: UILabel!
     @IBOutlet weak var multipleLable3: UILabel!
     @IBOutlet weak var multipleLable4: UILabel!
+    @IBOutlet weak var multipleSwitch1: UISwitch!
+    @IBOutlet weak var multipleSwitch2: UISwitch!
+    @IBOutlet weak var multipleSwitch3: UISwitch!
+    @IBOutlet weak var multipleSwitch4: UISwitch!
     
     @IBOutlet weak var rangedStackView: UIStackView!
     @IBOutlet weak var rangedLabel1: UILabel!
     @IBOutlet weak var rangedLabel2: UILabel!
+    @IBOutlet weak var rangedSlider: UISlider!
     
     @IBOutlet weak var progressView: UIProgressView!
+    
+    @IBAction func singleAnswerButtonPressed(_ sender: UIButton) {
+        let currentAnswers = questions[questionIndex].answers
+        
+        switch sender {
+        case singleButton1:
+            chosenAnswers.append(currentAnswers[0])
+        case singleButton2:
+            chosenAnswers.append(currentAnswers[1])
+        case singleButton3:
+            chosenAnswers.append(currentAnswers[2])
+        case singleButton4:
+            chosenAnswers.append(currentAnswers[3])
+        default:
+            break
+        }
+        
+        nextQuestion()
+    }
+    
+    @IBAction func multipleAnswerButtonPressed(_ sender: UIButton) {
+        let currentAnswers = questions[questionIndex].answers
+        
+        if multipleSwitch1.isOn {
+            chosenAnswers.append(currentAnswers[0])
+        }
+        if multipleSwitch2.isOn {
+            chosenAnswers.append(currentAnswers[1])
+        }
+        if multipleSwitch3.isOn {
+            chosenAnswers.append(currentAnswers[2])
+        }
+        if multipleSwitch4.isOn {
+            chosenAnswers.append(currentAnswers[3])
+        }
+        
+        nextQuestion()
+    }
+    
+    @IBAction func rangedAnswerButtonPressed(_ sender: UIButton) {
+        let currentAnswers = questions[questionIndex].answers
+        
+        let indexOfAnswerInSlider = Int(round(rangedSlider.value * Float(currentAnswers.count - 1)))
+        chosenAnswers.append(currentAnswers[indexOfAnswerInSlider])
+        
+        nextQuestion()
+    }
     
     var questions: [Question] = [
         Question(text: "What kind of food do you like?", type: .single, answers: [
@@ -52,12 +104,15 @@ class QuestionViewController: UIViewController {
     ]
     
     var questionIndex: Int = 0
+    
+    var chosenAnswers: [Answer] = [] // Массив для выбранных(ого) ответов(ответа) на текущий вопрос.
 
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
     }
     
+    // Функция для отображения Stack View, соотвествующего текущему вопросу.
     func updateUI() {
         singleStackView.isHidden = true
         multipleStackView.isHidden = true
@@ -67,7 +122,7 @@ class QuestionViewController: UIViewController {
         
         let currentQuestion = questions[questionIndex] // Текущий вопрос.
         let currentAnswers = currentQuestion.answers // Массив ответов на текущий вопрос.
-        let totalProgress = Float(questionIndex) / Float(questions.count) // Прогресс на текущий момент.
+        let totalProgress = Float(questionIndex + 1) / Float(questions.count) // Прогресс на текущий момент.
         
         switch currentQuestion.type {
         case .single:
@@ -102,5 +157,10 @@ class QuestionViewController: UIViewController {
         rangedStackView.isHidden = false
         rangedLabel1.text = answers.first?.text // Первый элемент массива.
         rangedLabel2.text = answers.last?.text // Последний элемент массива.
+    }
+    
+    // Функция для перехода на следующий вопрос.
+    func nextQuestion() {
+        
     }
 }
